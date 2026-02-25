a362ab51e9293aba2b28d668ba4b286db78cb1b4
Voxin <alexlogvinenko2007@gmail.com>
2026-02-25 16:30:49 +0800

    Refactor variable

--- mkdir/pinger.py
+++ mkdir/pinger.py
@@ -2,9 +2,9 @@ import requests
 from datetime import datetime, timedelta
 
 url_template="https://simurg.space/gen_file?data=obs&date={date}"
-now=datetime.now()
+current=datetime.now()
 while True:
-    date=now.strftime("%Y-%m-%d")
+    date=current.strftime("%Y-%m-%d")
     url=url_template.format(date=date)
 
     response=requests.get(url=url_template,stream=True)
@@ -14,4 +14,4 @@ while True:
         print(f"Last avaliable data are for {date}")
         break
     else:
-        now = now -timedelta(days=1)
+        current = current -timedelta(days=1)
