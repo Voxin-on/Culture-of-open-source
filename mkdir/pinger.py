23fcb281619cf68053a2a01b2fe6d95488cf0cd3
Voxin <alexlogvinenko2007@gmail.com>
2026-02-25 16:10:05 +0800

    add pinger.py

---
+++ mkdir/pinger.py
@@ -0,0 +1,8 @@
+import requests
+
+url_template="https://simurg.space/gen_file?data=obs&date={date}"
+date="2026-02-19"
+url=url_template.format(date=date)
+
+response=requests.get(url=url_template,stream=True)
+print(f"For {date}: responce")
