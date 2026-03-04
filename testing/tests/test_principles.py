8b0bba2d77b6e568260f72e72b8d546836f6eb61
Voxin <alexlogvinenko2007@gmail.com>
2026-03-04 16:15:25 +0800

    Show test not grant you right error

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -1,11 +1,18 @@
 import sys
 sys.path.append("../src")
-#TODO make it with ' pip instal -e .'
-from math_demo import add
+# TODO: make it with 'pip install -e .'
+from math_demo import add, add_with_bug
 
 def test_addition():
     assert add(2,2) == 4
-    print("Test: test_addition")
+    print("Test BASIC ADDITIONAL PASSED")
+    
+def test_addition_with_bug():
+    assert add_with_bug(2,2) == 4 
+    print("Test BUGGED ADDITIONAL PASSED")
+    #assert add_with_bug(3,3) == 6 but it will fail here
     
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
+    
