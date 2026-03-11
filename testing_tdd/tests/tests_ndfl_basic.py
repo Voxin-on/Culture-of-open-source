d6934ef1c42e249f09ed1143a5736a187905bea8
Voxin <alexlogvinenko2007@gmail.com>
2026-03-11 16:47:10 +0800

    Added new tests for ndfl basic

--- testing_tdd/tests/tests_ndfl_basic.py
+++ testing_tdd/tests/tests_ndfl_basic.py
@@ -1,4 +1,13 @@
 from ndfl import calculate_ndfl_tax
 
 def test_ndfl():
-    assert test_ndfl() is None
+    assert test_ndfl(0) is None
+    
+def test_ndfl_tier_1():
+    assert calculate_ndfl_tax(500_000) ==  65_000
+    
+def test_ndfl_tier_2():
+    assert calculate_ndfl_tax(4_000_000) ==  512_000
+    
+def test_ndfl_tier_3():
+    assert calculate_ndfl_tax(10_000_000) ==  1_002_000
