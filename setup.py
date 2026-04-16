0a70b8abfae7a8eabba370c95daa6f685462337b
Voxin <alexlogvinenko2007@gmail.com>
2026-04-16 19:06:28 +0800

    add url of github in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="vectors-calc",
     version="0.1.0",
     description="Simple vector operations: addition and scalar product",
+    url="https://github.com/Voxin-on/Culture-of-open-source",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
 )
