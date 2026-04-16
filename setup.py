33df77b2d6c5466c8fc7d2b5b1b99f8a748792af
Voxin <alexlogvinenko2007@gmail.com>
2026-04-16 19:11:53 +0800

    change version of package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vectors-calc",
-    version="0.1.0",
+    version="0.1.1",
     description="Simple vector operations: addition and scalar product",
     url="https://github.com/Voxin-on/Culture-of-open-source",
     package_dir={"": "src"},
