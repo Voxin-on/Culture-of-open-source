87873012f3f3b50032b1a4d092ff5e2670033633
Voxin <alexlogvinenko2007@gmail.com>
2026-04-16 19:30:04 +0800

    add long description connected with README

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="vectors-calc",
-    version="0.1.1",
+    version="0.1.2",
     description="Simple vector operations: addition and scalar product",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/Voxin-on/Culture-of-open-source",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
